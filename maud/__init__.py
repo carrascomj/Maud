3a7ee1ece4afbf445a8af5d2d550ede4eb5ec7a9
carrascomj <carrascomurielj@gmail.com>
2024-10-18 14:29:50 +0200

    refactor!: do not require stan

--- maud/__init__.py
+++ maud/__init__.py
@@ -8,13 +8,15 @@ from maud.running_stan import (
     load_stan_model,
 )
 
+# this is all commented so that Stan is not required
+
 # on Windows specifically, we should point cmdstanpy to the repackaged
 # CmdStan if it exists. This lets cmdstanpy handle the TBB path for us.
-local_cmdstan = STAN_FILES_FOLDER / f"cmdstan-{CMDSTAN_VERSION}"
-if os.path.exists(local_cmdstan):
-    cmdstanpy.set_cmdstan_path(str((local_cmdstan.resolve())))
+# local_cmdstan = STAN_FILES_FOLDER / f"cmdstan-{CMDSTAN_VERSION}"
+# if os.path.exists(local_cmdstan):
+#     cmdstanpy.set_cmdstan_path(str((local_cmdstan.resolve())))
 
-MODEL = load_stan_model("model", cpp_options={}, stanc_options={})
-OUT_OF_SAMPLE_MODEL = load_stan_model(
-    "out_of_sample_model", cpp_options={}, stanc_options={}
-)
+MODEL = None  # load_stan_model("model", cpp_options={}, stanc_options={})
+OUT_OF_SAMPLE_MODEL = None #load_stan_model(
+#     "out_of_sample_model", cpp_options={}, stanc_options={}
+# )
